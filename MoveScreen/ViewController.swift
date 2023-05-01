9f2f022bec10b1754ba7e15d737184010fe79dfb
Kim Eunji <qoopej@gmail.com>
2023-05-01 14:48:16 +0900

    cm

--- MoveScreen/ViewController.swift
+++ MoveScreen/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stepNumber: Int = 1 {
+        didSet {
+            self.title = "스텝넘버 : \(stepNumber)"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func onPushBtnClicked(_ sender: UIButton) {
+        
+        // 스토리보드 가져오기
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        // 뷰컨 가져오기
+        let vc = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        vc.stepNumber = stepNumber + 1
+        
+        // 푸시하기
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
 }
 
